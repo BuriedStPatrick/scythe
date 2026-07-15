8fd42df62ed96794e45168f5f6769e83308bbca3
Patrick Christensen <patrick@pat.gg>
2026-07-15 18:35:38 +0200

    Clean up

--- commands/osc/osc.command.ts
+++ commands/osc/osc.command.ts
@@ -1,6 +1,5 @@
 import type { ArgumentsCamelCase, Argv, CommandModule } from "yargs";
 import { oscSendCommand } from "./send.command";
-import { Buffer } from 'node:buffer'
 
 export const oscCommand: CommandModule = {
   command: 'osc',
@@ -8,52 +7,4 @@ export const oscCommand: CommandModule = {
   builder: args => args
     .command(oscSendCommand),
   handler: function (): void {}
-}
-
-export function parseOscTypeTag(type: string, argument: string): string | undefined {
-  switch (type) {
-    case 's':
-      return oscString(argument)
-    case 'n':
-      return oscFloat(Number.parseFloat(argument))
-    case 'f':
-      return oscFloat(Number.parseFloat(argument))
-    case 'i':
-        return oscInt32(Number.parseInt(argument))
-    case 'b': {
-      const value = (argument === 'true' || argument === '1')
-        ? 1
-        : 0
-
-      return oscInt32(value)
-    }
-    default:
-      return undefined
-  }
-}
-
-export function pad4(buffer: Buffer) {
-  const padding = (4 - (buffer.length % 4) % 4)
-
-  return Buffer.concat([buffer, Buffer.alloc(padding)])
-}
-
-export function oscString(value: string): Buffer {
-  return pad4(Buffer.from(`${value}\0`))
-}
-
-export function oscInt32(value: number): Buffer {
-  const buffer = Buffer.alloc(4)
-
-  buffer.writeInt32BE(value)
-
-  return buffer
-}
-
-export function oscFloat(value: number): Buffer {
-  const buffer = Buffer.alloc(4)
-
-  buffer.writeFloatBE(value)
-
-  return buffer
 }
--- osc/osc.ts
+++ osc/osc.ts
@@ -1,6 +1,5 @@
 import dgram from 'node:dgram'
 import { Buffer } from 'node:buffer'
-import { oscString, parseOscTypeTag } from '../commands/osc/osc.command'
 
 export type OscConnection = {
   host: string
@@ -34,4 +33,52 @@ export function send(command: OscCommand) {
       throw err
     }
   })
+}
+
+export function parseOscTypeTag(type: string, argument: string): string | undefined {
+  switch (type) {
+    case 's':
+      return oscString(argument)
+    case 'n':
+      return oscFloat(Number.parseFloat(argument))
+    case 'f':
+      return oscFloat(Number.parseFloat(argument))
+    case 'i':
+        return oscInt32(Number.parseInt(argument))
+    case 'b': {
+      const value = (argument === 'true' || argument === '1')
+        ? 1
+        : 0
+
+      return oscInt32(value)
+    }
+    default:
+      return undefined
+  }
+}
+
+export function pad4(buffer: Buffer) {
+  const padding = (4 - (buffer.length % 4) % 4)
+
+  return Buffer.concat([buffer, Buffer.alloc(padding)])
+}
+
+export function oscString(value: string): Buffer {
+  return pad4(Buffer.from(`${value}\0`))
+}
+
+export function oscInt32(value: number): Buffer {
+  const buffer = Buffer.alloc(4)
+
+  buffer.writeInt32BE(value)
+
+  return buffer
+}
+
+export function oscFloat(value: number): Buffer {
+  const buffer = Buffer.alloc(4)
+
+  buffer.writeFloatBE(value)
+
+  return buffer
 }
